d4ed846ac0d94feb56d01916b070a4a7a37794e9
leshless <leshless@leshless.com>
2026-01-06 19:04:31 +0100

    add ZipFunc and UnzipFunc to xmaps

--- xmaps/functions.go
+++ xmaps/functions.go
@@ -17,3 +17,23 @@ func Keys[K comparable, V any](m map[K]V) []K {
 
 	return res
 }
+
+func ZipFunc[K comparable, V any, KV any](s []KV, zipFunc func(kv KV) (K, V)) map[K]V {
+	res := make(map[K]V, len(s))
+	for _, kv := range s {
+		k, v := zipFunc(kv)
+		res[k] = v
+	}
+
+	return res
+}
+
+func UnzipFunc[K comparable, V any, KV any](m map[K]V, unzipFunc func(K, V) KV) []KV {
+	res := make([]KV, 0, len(m))
+	for k, v := range m {
+		kv := unzipFunc(k, v)
+		res = append(res, kv)
+	}
+
+	return res
+}
